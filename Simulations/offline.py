1f6b1982c776119b2f6a4f28148b534dbf5eef9c
annikahe <alhennes97@gmail.com>
2021-09-09 17:43:46 +0200

    Clean and document offline.py

--- Simulations/offline.py
+++ Simulations/offline.py
@@ -3,6 +3,25 @@ import matplotlib.pyplot as plt
 
 
 def h(i, x, prices, demands, phi):
+    """
+    We are looking for a solution that covers the requests up to time step i and has a full stock at the end of step i.
+    This function describes at what prices the stock, which is present at the end of time step i, was purchased.
+    The term 1 - h(i, x, prices, demands, phi) indicates the fraction of the stock
+    that was bought at a price of x or better.
+
+    :param i: integer
+        i in {0, ..., T}
+    :param x: float
+        some price x in [1, phi]
+    :param prices: list
+        complete list of all input prices
+    :param demands: list
+        complete list of all input demands
+    :param phi: float
+        upper bound on the prices, phi >= 1
+    :return: float
+        evaluation of the function h_i at x.
+    """
     if i == 0:
         if x == phi:
             return 0
@@ -16,6 +35,20 @@ def h(i, x, prices, demands, phi):
 
 
 def c_integral(i, prices, demands, phi):
+    """
+    Computes the cost that the optimal offline algorithm incurs to cover the demand v_i in time step i.
+
+    :param i: integer
+        time step, i in {0, ..., T}
+    :param prices: list
+        complete list of all input prices
+    :param demands: list
+        complete list of all input demands
+    :param phi: float
+        upper bound on the prices, phi >= 1
+    :return: float
+        cost that OPT incurs to cover v_i
+    """
     sorted_prices = [0] + sorted(prices)
     c_int = 0
     for j in range(i):
@@ -25,6 +58,18 @@ def c_integral(i, prices, demands, phi):
 
 
 def cost_opt(prices, demands, phi):
+    """
+    Computes the total cost of the optimal offline algorithm for the Economical Caching Problem.
+
+    :param prices: list
+        Complete list of prices (input)
+    :param demands: list
+        Complete list of demands (input)
+    :param phi: float
+        Upper bound on the prices.
+    :return: float
+        Total cost of OPT incurred during the whole input sequence.
+    """
     c_sum = 0
     for i in range(len(prices)):
         c_sum += c_integral(i + 1, prices, demands, phi)
