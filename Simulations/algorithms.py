39789b3fd92ca264f62110eeb997e8ff5fee8f7a
annikahe <alhennes97@gmail.com>
2021-10-11 23:41:58 +0200

    Delete instances.py and move its content to algorithms.py

--- Simulations/algorithms.py
+++ Simulations/algorithms.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Algorithm:
     """
     Basic framework for online algorithms for the Economical Caching Problem.
@@ -194,3 +196,23 @@ class AlgorithmRandom(Algorithm):
         lower_bound = np.max([0, demand - self.stock])
         upper_bound = 1 - self.stock + demand
         self.x = (upper_bound - lower_bound) * np.random.rand() + lower_bound
+
+
+class RPA(Algorithm):
+    """
+    Implementation of the "Reservationspreis"-algorithm with parameter sqrt(phi).
+    """
+    def buy(self, phi, price, demand):
+        if price <= np.sqrt(phi):
+            self.x = self.get_max_amount_buy(demand)
+        else:
+            self.x = 0
+
+
+class FtP(AlgorithmPred):
+    """
+    Implementation of the Follow-the-Prediction algorithm.
+    """
+    def buy(self, phi, price, demand):
+        self.update_prediction()
+        self.x = np.max([0, self.prediction - self.stock + demand])
