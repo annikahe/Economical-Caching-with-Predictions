269b77901ad7a65e7b7d6a228de305e8d65bbc9a
annikahe <alhennes97@gmail.com>
2021-10-17 23:47:49 +0200

    Work on offline.py

--- Simulations/offline.py
+++ Simulations/offline.py
@@ -1,5 +1,6 @@
 import history
 from algorithms import *
+import instances as inst
 
 from operator import itemgetter
 from decimal import *
@@ -8,6 +9,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from numpy.random import default_rng
 
+
+# compute optimal offline algorithm
 
 def take_from_stock(purchases, remaining_demand, current_step, current_price):
     purchases.sort(key=itemgetter(1))
@@ -33,7 +36,7 @@ def take_from_stock(purchases, remaining_demand, current_step, current_price):
     purchases.sort(key=itemgetter(0))
 
 
-def opt_decisions(prices, demands):
+def opt_stock_levels(prices, demands):
     # data structure: list of tuples
     # (time step, price, remaining space in stock, stock level)
 
@@ -57,39 +60,74 @@ def opt_decisions(prices, demands):
     return opt_stock_levels
 
 
-def compute_error(list1, list2):
-    error = 0
-    for i in range(len(list1)):
-        error += np.abs(list1[i] - list2[i])
+# Error with respect to optimal offline algorithm
 
-    return error / len(list1)
+def compute_error(opt, onl):
+    return np.sum(np.abs(opt[i] - onl[i]) for i in range(len(opt)))
 
 
-def moving_average(a, n=3) :
+# Upper bound on cost of FtP w.r.t OFF
+
+def bound_FtP_stock_error(phi, off, eta):
+    return off + np.min([phi * eta, (phi - 1)*(eta - 1) + phi])
+
+
+def comp_ratio_FtP_stock_error(phi, off, eta_norm):
+    """
+
+    Parameters
+    ----------
+    phi
+    off
+    eta_norm: normalized error eta/off
+
+    Returns
+    -------
+    competitive ratio for FtP for given normalized error (eta/off).
+    """
+    return 1 + np.min([phi * eta_norm, (phi - 1)*(eta_norm - 1/off) + phi/off])
+
+
+# plotting results
+
+def moving_average(a, window_size=3) :
     ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+    ret[window_size:] = ret[window_size:] - ret[:-window_size]
+    return ret[window_size - 1:] / window_size
 
 
-def plot_ratios(errors, ratios, window_size):
+def plot_ratios(errors, ratios, window_size, num_repetitions=1):
     df = pd.DataFrame({"Errors" : errors, "Ratios" : ratios})
     df = df.sort_values("Errors")
+    print(len(df))
+    df = df.drop_duplicates()
+    print(len(df))
     e = df['Errors'].values.tolist()
     r = df['Ratios'].values.tolist()
-    x = moving_average(e, window_size)
-    y = moving_average(r, window_size)
+    for i in range(num_repetitions):
+        e = moving_average(e, window_size)
+        r = moving_average(r, window_size)
     # print(moving_average(x, 200))
     # print(moving_average(y, 200))
 
-    plt.plot(x, y)
-    plt.show()
+    plt.plot(e, r)
+
+
+def create_predictions(method = "normal deviation", input_length = 0, deviation = 0,  rng = default_rng(), pred = []):
+    if method == "normal deviation":  # normal deviation from opt
+        return [np.clip(x + rng.normal(0, deviation), 0, 1) for x in pred]
+    elif method == "uniform random":  # place predictions uniform in [0,1]
+        return [rng.uniform() for x in range(input_length)]
+    elif method == "all 1":
+        return input_length * [1]
+    elif method == "all 0":
+        return input_length * [0]
 
 
 def quality_of_FtP(prices, demands, deviations, num_repetitions):
-
-    pred_opt_off = opt_decisions(prices, demands)
-
+    pred_opt_off = opt_stock_levels(prices, demands)
     phi = np.max(prices)
+    input_length = len(prices)
 
     eta1 = []
     eta2 = []
@@ -104,9 +142,24 @@ def quality_of_FtP(prices, demands, deviations, num_repetitions):
         alg_list = [opt]
 
         for d in deviations:
-            pred_opt_off_distorted = [np.clip(x + rng.normal(d), 0, 1) for x in pred_opt_off_copy]
+            #pred_opt_off_distorted = [np.clip(x + rng.normal(0, d), 0, 1) for x in pred_opt_off_copy]
+            pred_opt_off_distorted = create_predictions("normal deviation", input_length, d, rng, pred_opt_off_copy)
             distorted = FtP(0, 0, pred_opt_off_distorted)
             alg_list.append(distorted)
+
+        pred_random = create_predictions("uniform random", input_length, rng)
+        distorted = FtP(0, 0, pred_random)
+        alg_list.append(distorted)
+
+        # all 0 seems to be very good
+        pred_0 = create_predictions("all 0", input_length)
+        distorted = FtP(0, 0, pred_0)
+        alg_list.append(distorted)
+
+        # all 1 seems to be not as good as all 0
+        pred_1 = create_predictions("all 1", input_length)
+        distorted = FtP(0, 0, pred_1)
+        alg_list.append(distorted)
 
         algs_purchases, algs_acc_costs, algs_stocks, mindet_purchases, mindet_current_algs, mindet_acc_costs, mindet_stocks, mindet_cycles \
             = history.run_and_generate_history(phi, prices, demands, alg_list,
@@ -116,27 +169,25 @@ def quality_of_FtP(prices, demands, deviations, num_repetitions):
 
         for j in range(1, len(alg_list)):
             e1 = compute_error(algs_stocks[0], algs_stocks[j])
+            e1 /= algs_acc_costs[0][-1]
             eta1.append(e1)
-            print(e1)
 
             e2 = compute_error(algs_purchases[0], algs_purchases[j])
+            e2 /= algs_acc_costs[0][-1]
             eta2.append(e2)
-            # print(e2)
 
-            ratio = algs_acc_costs[1][-1] / algs_acc_costs[0][-1]
+            ratio = algs_acc_costs[j][-1] / algs_acc_costs[0][-1]
             ratios.append(ratio)
-            # print(ratio)
-
-    print(eta1)
-    print(eta2)
-    print(ratios)
 
     return eta1, eta2, ratios
 
 
-# opt_decisions([2,1,3,4,5,1], [0,0,0.2,1.6,0.4,0.2])
+# opt_stock_levels([2,1,3,4,5,1], [0,0,0.2,1.6,0.4,0.2])
 
-# opt_decisions([5,4,3,2,1,1,2,3,4,5], [0.1, 0.1, 0.2, 0, 0, 0,0.2, 0.1, 0.5, 0.5])
+# opt_stock_levels([5,4,3,2,1,1,2,3,4,5], [0.1, 0.1, 0.2, 0, 0, 0,0.2, 0.1, 0.5, 0.5])
+
+# TODO: compare optimal offline algorithm with that that always buys 0 or always buys 1
+# TODO: compare with completely random algorithm
 
 
 
