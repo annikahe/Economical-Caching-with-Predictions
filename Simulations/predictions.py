9d0bac985f0b22c7f9c1a5cf529407c21dce124a
annikahe <alhennes97@gmail.com>
2021-10-17 23:46:50 +0200

    Add predictions.py + add some prediction generators

---
+++ Simulations/predictions.py
@@ -0,0 +1,45 @@
+import numpy as np
+from numpy.random import default_rng
+
+
+# Generate predictions independently from the instance and the optimal solution
+
+def predictions_uniform(length):
+    rng = default_rng()
+    return [rng.uniform() for x in range(length)]
+
+
+def predictions_normal(length, mu=0.5, sigma=0.1):
+    rng = default_rng()
+    return [np.clip(rng.normal(mu, sigma), 0, 1) for x in range(length)]
+
+
+def predictions_0(length):
+    return [0] * len(length)
+
+
+def predictions_1(length):
+    return [1] * len(length)
+
+
+# Generate predictions based on the optimal offline solution
+
+def predictions_normal_off(off, sigma=0.1):
+    rng = default_rng()
+    return [np.clip(rng.normal(x, sigma), 0, 1) for x in off]
+
+
+# Other
+
+def pred_example1(num_repetitions):
+    pred_A0 = 4 * num_repetitions * [0]
+    pred_A1 = (num_repetitions - 3) * [1, 0] + (num_repetitions + 3) * [0, 1]
+    pred_A2 = [0, 1] + (num_repetitions - 2) * [1, 1] + (num_repetitions + 1) * [1, 0]
+    # Todo: return
+
+
+def pred_worst_case(num_repetitions):
+    pred_A0 = 5 * [0] + num_repetitions * [0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+    pred_A1 = [0, 1, 0, 1, 1] + num_repetitions * [1, 0, 1, 0, 0, 0, 1, 0, 1, 1]
+    pred_A2 = [0, 1, 0, 1, 0] + (2*num_repetitions) * [1, 0, 1, 0, 0]
+    # Todo: return
